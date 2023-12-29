7c83beedb42e1a4eb12d537553f710fc902502ff
Alecto <fomenkoandrey1978@gmail.com>
2023-12-29 21:19:55 +0200

    04

--- ! archive/04/main copy 2.js
+++ ! archive/04/main copy 2.js
@@ -12,3 +12,21 @@
 /* eslint-disable prefer-template */
 /* eslint-disable no-var */
 /* eslint-disable no-unused-vars */
+
+// function pow(x, n) {
+//   let res = x
+//   for (let i = 1; i < n; i++) {
+//     res *= x
+//   }
+//   return res
+// }
+
+function pow(x, n) {
+  if (n !== 1) {
+    return (x *= pow(x, n - 1))
+  }
+  return x
+}
+
+console.log(pow(2, 2)) // 4
+console.log(pow(2, 3))// 8
